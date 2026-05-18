cce56de10118203ea20e8d023d9b021be5bda5da
nicheworks-tools <nicheworks.tools@gmail.com>
2026-05-18 11:44:59 +0900

    Load CTA image pilots batch 008

--- tools/construction-tools-atlas/detail-image-hotfix.js
+++ tools/construction-tools-atlas/detail-image-hotfix.js
@@ -8,7 +8,8 @@
     "./data/image-pilots-004.json?v=20260513-asset-8",
     "./data/image-pilots-005.json?v=20260513-asset-9",
     "./data/image-pilots-006.json?v=20260513-asset-10",
-    "./data/image-pilots-007.json?v=20260513-asset-11"
+    "./data/image-pilots-007.json?v=20260513-asset-11",
+    "./data/image-pilots-008.json?v=20260513-asset-12"
   ];
 
   const state = {
