91e2b90757772876d12e17700f338249ba080c56
nicheworks-tools <nicheworks.tools@gmail.com>
2026-05-25 22:55:24 +0900

    feat: aggregate Pattern Atlas seed datasets

---
+++ tools/pattern-atlas/js/data/patterns-all.js
@@ -0,0 +1,9 @@
+import { patterns as basePatterns } from './patterns.js';
+import { patternsExtra01 } from './patterns-extra-01.js';
+import { patternsExtra02 } from './patterns-extra-02.js';
+
+export const patterns = [
+  ...basePatterns,
+  ...patternsExtra01,
+  ...patternsExtra02
+];
