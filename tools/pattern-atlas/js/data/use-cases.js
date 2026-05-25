e1e51e3ca7bd35024eb442e1eb57de7adb606179
nicheworks-tools <nicheworks.tools@gmail.com>
2026-05-25 20:28:38 +0900

    feat: add Pattern Atlas use case data

---
+++ tools/pattern-atlas/js/data/use-cases.js
@@ -0,0 +1,10 @@
+export const useCases = [
+  { id: 'web-background', nameEn: 'Web background', nameJa: 'Web背景' },
+  { id: 'textile', nameEn: 'Textile', nameJa: '織物・布地' },
+  { id: 'poster', nameEn: 'Poster', nameJa: 'ポスター' },
+  { id: 'packaging', nameEn: 'Packaging', nameJa: 'パッケージ' },
+  { id: 'card', nameEn: 'Card', nameJa: 'カード' },
+  { id: 'wallpaper', nameEn: 'Wallpaper', nameJa: '壁紙' },
+  { id: 'presentation', nameEn: 'Presentation', nameJa: 'プレゼン背景' },
+  { id: 'education', nameEn: 'Education / reference', nameJa: '学習・資料' }
+];
