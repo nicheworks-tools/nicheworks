3eeb6d75c75e9278e13781a513f27d6b847c49ac
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-12 18:48:12 +0900

    ManualFinder: add verified Seiko and Roland Wave 2B records

---
+++ tools/manual-finder/data/manuals.wave2.04.js
@@ -0,0 +1,18 @@
+(window.MANUALFINDER_WAVE2_SEIKO ||= []).push(`H021|https://www.seikowatches.com/us-en/-/media/Files/Common/Seiko/instructions/Japanese/H/H021/H021.pdf
+H022|https://www.seikowatches.com/us-en/-/media/Files/Common/Seiko/instructions/Japanese/H/H022/H022.pdf
+H023|https://www.seikowatches.com/us-en/-/media/Files/Common/Seiko/instructions/Japanese/H/H023/H023.pdf
+H024|https://www.seikowatches.com/us-en/-/media/Files/Common/Seiko/instructions/Japanese/H/H024/H024.pdf
+H851|https://www.seikowatches.com/instructions/html/SEIKO_H851_D_JP/index
+H855|https://www.seikowatches.com/instructions/html/SEIKO_H855_D_JP/index`);
+(window.MANUALFINDER_WAVE2_ROLAND ||= []).push(`N-835|https://lib.roland.co.jp/support/jp/manuals/res/1808910/N-835_j1.pdf
+NE-7|https://lib.roland.co.jp/support/jp/manuals/res/1809117/NE-7_je1.pdf
+NF-1|https://lib.roland.co.jp/support/jp/manuals/res/1809519/NF-1_j.pdf
+NS-50|https://lib.roland.co.jp/support/jp/manuals/res/1809116/NS-50_j.pdf
+OC-2|https://lib.roland.co.jp/support/jp/manuals/res/1809767/OC-2_j2.pdf
+OC-20G|https://lib.roland.co.jp/support/jp/manuals/res/1810530/OC-20G_j1.pdf
+OD-1|https://lib.roland.co.jp/support/jp/manuals/res/1808908/OD-1_j.pdf
+OD-2|https://lib.roland.co.jp/support/jp/manuals/res/1809115/OD-2_j.pdf
+OD-2R|https://lib.roland.co.jp/support/jp/manuals/res/1809114/OD-2R_j.pdf
+P-1|https://lib.roland.co.jp/support/jp/manuals/res/1810528/P-1_r_j2.pdf
+P-330|https://lib.roland.co.jp/support/jp/manuals/res/1808903/P-330_j.pdf
+P-55|https://lib.roland.co.jp/support/jp/manuals/res/1810023/P-55_j.pdf`);
