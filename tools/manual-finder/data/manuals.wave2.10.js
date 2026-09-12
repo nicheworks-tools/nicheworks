6785239a902ef0a2df05e77f3d35589851c045f7
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-13 00:51:05 +0900

    ManualFinder: add Seiko Wave 2AH V-series residual coverage (#603)
    * ManualFinder: add Seiko Wave 2AH V-series manuals
    
    * ManualFinder: load Wave 2AH data in Japanese UI
    
    * ManualFinder: load Wave 2AH data in English UI

---
+++ tools/manual-finder/data/manuals.wave2.10.js
@@ -0,0 +1,25 @@
+// Wave 2AH: residual direct Seiko V-series Japanese manuals; shared and multi-variant targets are excluded.
+(window.MANUALFINDER_WAVE2_SEIKO ||= []).push(`VC01|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VC01/VC01.pdf
+VC10|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VC10/VC10.pdf
+VC11|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VC11/VC11.pdf
+VD53|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VD53/VD53.pdf
+VD54|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VD54/VD54.pdf
+VD57|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VD57/VD57.pdf
+VD73|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VD73/VD73.pdf
+VD75|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VD75/VD75.pdf
+VD76|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VD76/VD76.pdf
+VD77|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VD77/VD77.pdf
+VD78|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VD78/VD78.pdf
+VH31|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VH31/VH31.pdf
+VH67|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VH67/VH67.pdf
+VJ12|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VJ12/VJ12.pdf
+VJ20|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VJ20/VJ20.pdf
+VJ21|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VJ21/VJ21.pdf
+VJ22|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VJ22/VJ22.pdf
+VJ23|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VJ23/VJ23.pdf
+VJ32|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VJ32/VJ32.pdf
+VJ33|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VJ33/VJ33.pdf
+VJ42|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VJ42/VJ42.pdf
+VJ46|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VJ46/VJ46.pdf
+VJ47|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VJ47/VJ47.pdf
+VJ48|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/V/VJ48/VJ48.pdf`);
