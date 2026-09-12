b852c802314627b0712eb7314a55f73ff05763c4
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-12 19:09:31 +0900

    ManualFinder: extend Roland Wave 2E direct-manual coverage (#540)
    * ManualFinder: add Roland Wave 2E direct manuals
    
    * ManualFinder: preload Roland Wave 2E on Japanese page
    
    * ManualFinder: preload Roland Wave 2E on English page

---
+++ tools/manual-finder/data/manuals.wave2.07.js
@@ -0,0 +1,21 @@
+(window.MANUALFINDER_WAVE2_ROLAND ||= []).push(`PCR-A30|https://lib.roland.co.jp/support/jp/manuals/res/1810527/PCR-A30_j2.pdf
+PD-1|https://lib.roland.co.jp/support/jp/manuals/res/1808714/PD-1_j_.pdf
+PD-5|https://lib.roland.co.jp/support/jp/manuals/res/1808716/PD-5_je2.pdf
+PG-10|https://lib.roland.co.jp/support/jp/manuals/res/1808717/PG-10_j.pdf
+PG-1000|https://lib.roland.co.jp/support/jp/manuals/res/1808718/PG-1000_j.pdf
+PG-200|https://lib.roland.co.jp/support/jp/manuals/res/1808719/PG-200_j.pdf
+PG-300|https://lib.roland.co.jp/support/jp/manuals/res/1808720/PG-300_j.pdf
+PG-800|https://lib.roland.co.jp/support/jp/manuals/res/1808721/PG-800_j.pdf
+PH-830|https://lib.roland.co.jp/support/jp/manuals/res/100002/PH-830_j.pdf
+PS-3|https://lib.roland.co.jp/support/jp/manuals/res/1809947/PS-3_j.pdf
+PK-25|https://lib.roland.co.jp/support/jp/manuals/res/1810510/PK-25_j4.pdf
+PK-5|https://lib.roland.co.jp/support/jp/manuals/res/1809829/PK-5_j.pdf
+PK-6|https://lib.roland.co.jp/support/jp/manuals/res/62472952/PK-6_j01_W.pdf
+PK-7|https://lib.roland.co.jp/support/jp/manuals/res/1810512/PK-7_j4.pdf
+PM-1|https://lib.roland.co.jp/support/jp/manuals/res/1810513/PM-1_j1.pdf
+PM-16|https://lib.roland.co.jp/support/jp/manuals/res/1809515/PM-16_j.pdf
+PN-2|https://lib.roland.co.jp/support/jp/manuals/res/1808722/PN-2_j.pdf
+PN-D50|https://lib.roland.co.jp/support/jp/manuals/res/1808723/PN-D50-01_j.pdf
+PQ-3B|https://lib.roland.co.jp/support/jp/manuals/res/1808724/PQ-3B_j.pdf
+PQ-4|https://lib.roland.co.jp/support/jp/manuals/res/1809583/PQ-4_j.pdf
+PQ-50|https://lib.roland.co.jp/support/jp/manuals/res/1809591/PQ-50_j.pdf`);
