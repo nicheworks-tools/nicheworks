25e2841787579c0cf1934f908f4ab01990a12eb4
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-13 15:01:40 +0900

    ManualFinder: add Sony Wave 3C alpha flagship manuals

---
+++ tools/manual-finder/data/manuals.wave3.03.js
@@ -0,0 +1,6 @@
+// Wave 3C: complete current Sony α1 and α9 E-mount body series from the official Japanese series/manual pages verified 2026-09-13.
+(window.MANUALFINDER_WAVE3_SONY ||= []).push(`ILCE-1M2|https://support.sony.jp/electronics/support/e-mount-body-ilce-1-series/ilce-1m2/manuals
+ILCE-1|https://support.sony.jp/electronics/support/e-mount-body-ilce-1-series/ilce-1/manuals
+ILCE-9M3|https://support.sony.jp/electronics/support/e-mount-body-ilce-9-series/ilce-9m3/manuals
+ILCE-9M2|https://support.sony.jp/electronics/support/e-mount-body-ilce-9-series/ilce-9m2/manuals
+ILCE-9|https://support.sony.jp/electronics/support/e-mount-body-ilce-9-series/ilce-9/manuals`);
