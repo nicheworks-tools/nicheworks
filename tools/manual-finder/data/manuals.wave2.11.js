170281d2e00a9598b623030cc9d6f7a64fb4a9f6
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-13 09:32:21 +0900

    ManualFinder: add Seiko Wave 2AM Y-series manuals

---
+++ tools/manual-finder/data/manuals.wave2.11.js
@@ -0,0 +1,22 @@
+// Wave 2AM: direct Seiko Y-series Japanese primary manuals; English variants and YK70 software are omitted.
+(window.MANUALFINDER_WAVE2_SEIKO ||= []).push(`YK39|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/D/YK39/DYK39_instructions.pdf
+YK50|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/D/YK50/DYK50_instructions.pdf
+YK51|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/D/YK51/DYK51_instructions.pdf
+YK52|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/D/YK52/DYK52_instructions.pdf
+Y121|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y121/Y121.pdf
+Y136|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y136/Y136.pdf
+Y150|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y150/Y150.pdf
+Y151|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y151/Y151.pdf
+Y15W|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y15W/Y15W.pdf
+Y182|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y182/Y182.pdf
+Y520|https://www.seikowatches.com/middleeast-en/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y520/Y520.pdf
+Y675|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y675/Y675.pdf
+Y676|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y676/Y676.pdf
+Y717|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y717/Y717.pdf
+Y731|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y731/Y731.pdf
+Y735|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/Y735/Y735.pdf
+YD03|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/YD03/YD03.pdf
+YK54|https://www.seikowatches.com/middleeast-en/-/media/Files/Common/Seiko/instructions/Japanese/Y/YK54/YK54_2.pdf
+YK70|https://www.seikowatches.com/middleeast-en/-/media/Files/Common/Seiko/instructions/Japanese/Y/YK70/YK70_3.pdf
+YL50|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/YL50/YL50.pdf
+YM92|https://www.seikowatches.com/jp-ja/-/media/Files/Common/Seiko/instructions/Japanese/Y/YM92/YM92.pdf`);
