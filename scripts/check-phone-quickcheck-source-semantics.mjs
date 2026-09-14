d7530e7008b5058512a2fd1bbf94674a787b8cb3
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-15 01:39:29 +0900

    Phone QuickCheck: add nubia Fold

--- scripts/check-phone-quickcheck-source-semantics.mjs
+++ scripts/check-phone-quickcheck-source-semantics.mjs
@@ -16,7 +16,7 @@ const trustedSourceDomains = {
   OPPO: ['oppo.com'],
   Xiaomi: ['mi.com'],
   Motorola: ['motorola.com', 'motorola.co.jp'],
-  ZTE: ['nubia.com', 'ymobile.jp']
+  ZTE: ['nubia.com', 'ymobile.jp', 'softbank.jp']
 };
 
 function hostnameOf(value) {
