eff23cc9f9f9c6e2a298b3fdff5be886f0ce211d
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-14 22:14:42 +0900

    Cosmetics PR53: add verified category provenance wave 4

--- tools/_shared/cosmetic-ingredient-parser.js
+++ tools/_shared/cosmetic-ingredient-parser.js
@@ -113,6 +113,16 @@
       category: "chelating agent",
       sources: Object.freeze(["https://www.cosmeticsinfo.org/ingredient/disodium-edta/"]),
       authority: "Personal Care Products Council / Cosmetics Info"
+    }),
+    "tocopheryl acetate": Object.freeze({
+      category: "antioxidant",
+      sources: Object.freeze(["https://www.cosmeticsinfo.org/ingredient/tocopherol/"]),
+      authority: "Personal Care Products Council / Cosmetics Info"
+    }),
+    "sodium citrate": Object.freeze({
+      category: "pH adjuster",
+      sources: Object.freeze(["https://www.cosmeticsinfo.org/ingredient/citric-acid/"]),
+      authority: "Personal Care Products Council / Cosmetics Info"
     })
   });
 
@@ -563,7 +573,7 @@
   }
 
   const api = {
-    version: "1.20.0",
+    version: "1.21.0",
     normalizeText,
     normalizeBaseKey,
     normalizeKey,
