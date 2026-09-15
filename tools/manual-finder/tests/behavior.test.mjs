b48709b8f4dc507f6039b59a80405db016507a33
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-15 10:59:21 +0900

    ManualFinder: add KYOCERA toner Wave 8

--- tools/manual-finder/tests/behavior.test.mjs
+++ tools/manual-finder/tests/behavior.test.mjs
@@ -7,4 +7,5 @@ await import('./oki-toner-wave4.test.mjs');
 await import('./oki-toner-wave5.test.mjs');
 await import('./oki-toner-wave6.test.mjs');
 await import('./oki-ribbon-wave1.test.mjs');
+await import('./kyocera-toner-wave8.test.mjs');
 await import('./affiliate-coverage.test.mjs');
