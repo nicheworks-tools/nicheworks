ca2f515c1c70d8f1db8165f0f08b2ea1ab7f8add
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-16 21:54:25 +0900

    ManualFinder: load DJI Mini accessory Wave 3 before render

--- tools/manual-finder/affiliate-runtime.js
+++ tools/manual-finder/affiliate-runtime.js
@@ -34,7 +34,13 @@
                 loadScript(
                   "affiliate-dji-camera-accessories-wave2.js?v=mf-dji-camera-accessory-wave2-20260916a",
                   () => Boolean(window.MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE2_LEDGER),
-                  done
+                  () => {
+                    loadScript(
+                      "affiliate-dji-camera-accessories-wave3.js?v=mf-dji-camera-accessory-wave3-20260916a",
+                      () => Boolean(window.MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE3_LEDGER),
+                      done
+                    );
+                  }
                 );
               }
             );
