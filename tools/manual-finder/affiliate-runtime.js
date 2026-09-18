1b53542678ad20c578a5eed28617836e43868c5e
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-18 20:22:20 +0900

    ManualFinder: load DJI Goggles 2 Wave 31 runtime

--- tools/manual-finder/affiliate-runtime.js
+++ tools/manual-finder/affiliate-runtime.js
@@ -51,7 +51,8 @@
       ["affiliate-dji-camera-accessories-wave27.js?v=mf-dji-camera-accessory-wave27-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE27_LEDGER"],
       ["affiliate-dji-camera-accessories-wave28.js?v=mf-dji-camera-accessory-wave28-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE28_LEDGER"],
       ["affiliate-dji-camera-accessories-wave29.js?v=mf-dji-camera-accessory-wave29-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE29_LEDGER"],
-      ["affiliate-dji-camera-accessories-wave30.js?v=mf-dji-camera-accessory-wave30-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE30_LEDGER"]
+      ["affiliate-dji-camera-accessories-wave30.js?v=mf-dji-camera-accessory-wave30-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE30_LEDGER"],
+      ["affiliate-dji-camera-accessories-wave31.js?v=mf-dji-camera-accessory-wave31-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE31_LEDGER"]
     ];
 
     let index = 0;
