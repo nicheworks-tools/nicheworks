0bf8f3f05592a6f80735e31871bfa99247e2f207
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-18 14:51:32 +0900

    ManualFinder: load DJI Action 2 Wave 27 runtime

--- tools/manual-finder/affiliate-runtime.js
+++ tools/manual-finder/affiliate-runtime.js
@@ -47,7 +47,8 @@
       ["affiliate-dji-camera-accessories-wave23.js?v=mf-dji-camera-accessory-wave23-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE23_LEDGER"],
       ["affiliate-dji-camera-accessories-wave24.js?v=mf-dji-camera-accessory-wave24-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE24_LEDGER"],
       ["affiliate-dji-camera-accessories-wave25.js?v=mf-dji-camera-accessory-wave25-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE25_LEDGER"],
-      ["affiliate-dji-camera-accessories-wave26.js?v=mf-dji-camera-accessory-wave26-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE26_LEDGER"]
+      ["affiliate-dji-camera-accessories-wave26.js?v=mf-dji-camera-accessory-wave26-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE26_LEDGER"],
+      ["affiliate-dji-camera-accessories-wave27.js?v=mf-dji-camera-accessory-wave27-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE27_LEDGER"]
     ];
 
     let index = 0;
