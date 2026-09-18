022d7a455b5d8dd5337318a8b4f72be9afbeaa75
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-18 12:59:50 +0900

    ManualFinder: load DJI camera accessory Wave 25

--- tools/manual-finder/affiliate-runtime.js
+++ tools/manual-finder/affiliate-runtime.js
@@ -45,7 +45,8 @@
       ["affiliate-dji-camera-accessories-wave21.js?v=mf-dji-camera-accessory-wave21-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE21_LEDGER"],
       ["affiliate-dji-camera-accessories-wave22.js?v=mf-dji-camera-accessory-wave22-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE22_LEDGER"],
       ["affiliate-dji-camera-accessories-wave23.js?v=mf-dji-camera-accessory-wave23-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE23_LEDGER"],
-      ["affiliate-dji-camera-accessories-wave24.js?v=mf-dji-camera-accessory-wave24-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE24_LEDGER"]
+      ["affiliate-dji-camera-accessories-wave24.js?v=mf-dji-camera-accessory-wave24-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE24_LEDGER"],
+      ["affiliate-dji-camera-accessories-wave25.js?v=mf-dji-camera-accessory-wave25-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE25_LEDGER"]
     ];
 
     let index = 0;
