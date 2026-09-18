1448ba3d9168216457252cd66d6af861ed4bf70c
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-18 23:26:04 +0900

    ManualFinder: load Ronin grip Wave 35 runtime

--- tools/manual-finder/affiliate-runtime.js
+++ tools/manual-finder/affiliate-runtime.js
@@ -53,7 +53,8 @@
       ["affiliate-dji-camera-accessories-wave29.js?v=mf-dji-camera-accessory-wave29-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE29_LEDGER"],
       ["affiliate-dji-camera-accessories-wave30.js?v=mf-dji-camera-accessory-wave30-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE30_LEDGER"],
       ["affiliate-dji-camera-accessories-wave31.js?v=mf-dji-camera-accessory-wave31-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE31_LEDGER"],
-      ["affiliate-dji-camera-accessories-wave33.js?v=mf-dji-camera-accessory-wave33-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE33_LEDGER"]
+      ["affiliate-dji-camera-accessories-wave33.js?v=mf-dji-camera-accessory-wave33-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE33_LEDGER"],
+      ["affiliate-dji-camera-accessories-wave35.js?v=mf-dji-camera-accessory-wave35-20260918a", "MANUALFINDER_DJI_CAMERA_ACCESSORY_WAVE35_LEDGER"]
     ];
 
     let index = 0;
