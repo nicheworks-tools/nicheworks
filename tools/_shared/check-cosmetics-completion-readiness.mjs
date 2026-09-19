cabf804af36cb3645ff72f6544a05f6643469bae
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-19 13:42:00 +0900

    Cosmetics: ratchet verified note readiness to 74

--- tools/_shared/check-cosmetics-completion-readiness.mjs
+++ tools/_shared/check-cosmetics-completion-readiness.mjs
@@ -53,8 +53,8 @@ const BASELINE_FLOORS = Object.freeze({
 
 const BASELINE_RUNTIME_PROVENANCE_FLOORS = Object.freeze({
   verified_category_runtime_identities: 119,
-  verified_note_runtime_identities: 70,
-  strong_runtime_data_identities: 70
+  verified_note_runtime_identities: 74,
+  strong_runtime_data_identities: 74
 });
 
 function text(value = '') {
