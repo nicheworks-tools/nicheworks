0a425dd89a499430b8079734b1e01dc278d391f1
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-19 16:19:23 +0900

    ManualFinder: load Apple iPhone coverage-pass batch

--- tools/manual-finder/data/manuals.coverage-passes.js
+++ tools/manual-finder/data/manuals.coverage-passes.js
@@ -3,11 +3,13 @@
 
   window.MANUALFINDER_COVERAGE_PASS_BATCHES = Object.freeze([
     "manuals.coverage-pass.sony-jp-alpha-e-mount.js",
-    "manuals.coverage-pass.panasonic-jp-lumix-camera-bodies.js"
+    "manuals.coverage-pass.panasonic-jp-lumix-camera-bodies.js",
+    "manuals.coverage-pass.apple-jp-iphone-ios26-guide-models.js"
   ]);
 
   window.MANUALFINDER_BUILD_COVERAGE_PASSES = () => [
     ...(window.MANUALFINDER_COVERAGE_PASS_SONY_EMOUNT || []),
-    ...(window.MANUALFINDER_COVERAGE_PASS_PANASONIC_LUMIX || [])
+    ...(window.MANUALFINDER_COVERAGE_PASS_PANASONIC_LUMIX || []),
+    ...(window.MANUALFINDER_COVERAGE_PASS_APPLE_IPHONE || [])
   ];
 })();
