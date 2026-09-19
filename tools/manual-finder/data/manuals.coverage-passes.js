24930212fb5ac6e7e07a7eab133ebeaf79bba731
nicheworks-tools <nicheworks.tools@gmail.com>
2026-09-19 12:55:52 +0900

    ManualFinder: load Panasonic LUMIX coverage-pass batch

--- tools/manual-finder/data/manuals.coverage-passes.js
+++ tools/manual-finder/data/manuals.coverage-passes.js
@@ -2,10 +2,12 @@
   "use strict";
 
   window.MANUALFINDER_COVERAGE_PASS_BATCHES = Object.freeze([
-    "manuals.coverage-pass.sony-jp-alpha-e-mount.js"
+    "manuals.coverage-pass.sony-jp-alpha-e-mount.js",
+    "manuals.coverage-pass.panasonic-jp-lumix-camera-bodies.js"
   ]);
 
   window.MANUALFINDER_BUILD_COVERAGE_PASSES = () => [
-    ...(window.MANUALFINDER_COVERAGE_PASS_SONY_EMOUNT || [])
+    ...(window.MANUALFINDER_COVERAGE_PASS_SONY_EMOUNT || []),
+    ...(window.MANUALFINDER_COVERAGE_PASS_PANASONIC_LUMIX || [])
   ];
 })();
